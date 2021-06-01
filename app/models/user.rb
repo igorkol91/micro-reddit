f12da45d1f12dde54e613201761b236f7635f72f
safafa <safaaballagh97@gmail.com>
2021-06-01 10:34:01 +0100

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ApplicationRecord
-    has_many :post
-    has_many :comment
-    validates :username, presence: true
-    validates_uniqueness_of :username
+
+  has_many :post
+
+
+
+  has_many :comment
+
+
+
+  validates :username, presence: true
+
+
+
+  validates_uniqueness_of :username
+
+  validates_length_of :username, minimum: 4, maximum: 12
+
 end
+
